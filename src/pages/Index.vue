696ea5f415e240e2f0dbad1a9efd7a47489d2090
OmairMedia <omairmedia2020@gmail.com>
2022-03-13 14:51:31 +0500

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-page class="">
+    <!-- Carousal Section -->
+    <q-carousel
+      arrows
+      animated
+      control-type="push"
+      v-model="slide"
+      height="90vh"
+    >
+      <q-carousel-slide
+        name="first"
+        img-src="https://cdn.quasar.dev/img/mountains.jpg"
+      >
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">First stop</div>
+          <div class="text-subtitle1">Mountains</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide
+        name="second"
+        img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+      >
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Second stop</div>
+          <div class="text-subtitle1">Famous City</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide
+        name="third"
+        img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+      >
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Third stop</div>
+          <div class="text-subtitle1">Famous Bridge</div>
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { ref } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+  setup() {
+    return {
+      slide: ref("first"),
+    };
+  },
+});
+</script>
+
+
+<style scoped>
+.custom-caption {
+  text-align: center;
+  padding: 12px;
+  color: white;
+}
+</style>
